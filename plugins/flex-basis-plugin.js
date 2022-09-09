649fce7b67f3c5e92e0e801af2c25fc99753b0ea
Oliver Davies <oliver@oliverdavies.uk>
2022-09-09 18:00:00 +0100

    refactor: flex basis plugin with options

--- plugins/flex-basis-plugin.js
+++ plugins/flex-basis-plugin.js
@@ -1,11 +1,21 @@
 let plugin = require("tailwindcss/plugin");
 
-let flexBasisPlugin = plugin(function ({ addBase, addUtilities }) {
-  let values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let flexBasisPlugin = plugin.withOptions(
+  function (options) {
+    return function ({ addUtilities }) {
+      let values = options?.values ?? [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-  values.forEach((value) => {
-    addUtilities({ [`.flex-basis-${value}`]: { flexBasis: `${value}%` } });
-  });
-});
+      values.forEach((value) => {
+        addUtilities({ [`.flex-basis-${value}`]: { flexBasis: `${value}%` } });
+      });
+    };
+  },
+
+  function (options) {
+    return {
+      values: options?.values ?? [],
+    };
+  }
+);
 
 module.exports = flexBasisPlugin;
